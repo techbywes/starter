3cd42144f695ec9fac5e7d0ec749653305fa3255
techbywes <dhspotless@gmail.com>
2023-04-04 11:04:05 +0000

    new commit

--- script.js
+++ script.js
@@ -5,32 +5,59 @@ let overLay_El = document.querySelector('.overlay');
 let closeModalBtn = document.querySelector('.close-modal');
 let openModelBtn = document.querySelectorAll('.show-modal');
 
-let openModel = function () {
+
+
+// let openModel = function () {
+//     modal_El.classList.remove('hidden');
+//     overLay_El.classList.remove('hidden');
+// }
+
+// for (let i = 0; i < openModelBtn.length; i++)
+//     openModelBtn[i].addEventListener('click', openModel)
+
+// let closeModal = function () {
+//     modal_El.classList.add('hidden');
+//     overLay_El.classList.add('hidden');
+// }
+// closeModalBtn.addEventListener('click', closeModal);
+// overLay_El.addEventListener('click', closeModal);
+
+// document.addEventListener('keydown', function (e) {
+//     if (e.key === 'Escape' && !modal_El.classList.contains('hidden')) {
+//             closeModal();
+//         }
+// });
+
+
+//ASSIGNMENT DONE
+
+let openModel = function ()
+{
     modal_El.classList.remove('hidden');
     overLay_El.classList.remove('hidden');
 }
 
+for (let i = 0; i < openModelBtn.length; i++) 
+{
+   openModelBtn[i].addEventListener('click', openModel)
+}
 
-for (let i = 0; i < openModelBtn.length; i++)
-    openModelBtn[i].addEventListener('click', openModel)
-
-
-let closeModal = function () {
+let closeModal = function ()
+{
     modal_El.classList.add('hidden');
     overLay_El.classList.add('hidden');
 }
-closeModalBtn.addEventListener('click', closeModal);
+closeModalBtn.addEventListener ('click', closeModal)
 overLay_El.addEventListener('click', closeModal);
 
-document.addEventListener('keydown', function (e) {
-    if (e.key === 'Escape' && !modal_El.classList.contains('hidden')) {
-            // this code means if the modal does not contain the hidden class execute the function to close the modal.
-            closeModal();
-        }
-});
+document.addEventListener('keydown', function (event) {
+    if( event.key === 'Escape' && !modal_El.classList.contains('hidden'))
+    {
+        closeModal();
+    }
+})
 
 
-// closeModalBtn.addEventListener('click', function () {
-//     modal_El.classList.add('hidden');
-//     overLay_El.classList.add('hidden');
-// })
+
+
+
